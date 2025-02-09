296a26a70aa0fa712f0ca2c17b6b34615a38093c
YakshitAgarwal <yakshitagarwal05@gmail.com>
2025-02-09 20:44:55 +0530

    eval done correct

--- frontend/src/app/battle-royale/[id]/page.tsx
+++ frontend/src/app/battle-royale/[id]/page.tsx
@@ -71,12 +71,7 @@ import { Heart, DollarSign } from "lucide-react";
 import TextPressure from "@/app/components/textPressure";
 import LiveChat from "@/app/components/liveChat";
 import { useParams } from "next/navigation";
-<<<<<<< HEAD
 import { getRoomById } from "@/db/mongodb";
-=======
-//import { getRoomById } from '@/db/mongodb';
-
->>>>>>> origin
 
 // Import ethers directly from the browser-compatible package
 import { ethers } from "ethers";
@@ -92,22 +87,12 @@ export default function BattleRoyale() {
   const params = useParams();
   const roomId = params.id; // The dynamic route parameter
 
-<<<<<<< HEAD
   if (typeof roomId === "string") {
     const fight = "";
     console.log(fight);
   } else {
     console.error("Invalid roomId:", roomId);
   }
-=======
-	if (typeof roomId === 'string') {
-	//  const fight = getRoomById(roomId);
-	//   console.log(fight);
-	} else {
-	  console.error('Invalid roomId:', roomId);
-	}
-	
->>>>>>> origin
 
   const contractAddress = "0x97490eb90f2be6d6cbaf75951105ff1113779669";
   const contractABI = [
@@ -417,7 +402,6 @@ export default function BattleRoyale() {
     },
   ];
 
-<<<<<<< HEAD
   //   async function placeBet(personality: 1 | 2, betAmount: string) {
   //     setIsLoading(true);
   //     setError("");
@@ -431,317 +415,8 @@ export default function BattleRoyale() {
   //   },
   //   body: JSON.stringify({ personality, betAmount }),
   // });
-=======
- //'0x97490eb90f2be6d6cbaf75951105ff1113779669'
 
-  const contractAddress = "0x34040646ba5166C6Df72Eb82d754AcF9EaCe5724";
-  const contractABI = [
-	{
-		"inputs": [
-			{
-				"internalType": "uint256",
-				"name": "_duration",
-				"type": "uint256"
-			}
-		],
-		"stateMutability": "nonpayable",
-		"type": "constructor"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "address",
-				"name": "owner",
-				"type": "address"
-			}
-		],
-		"name": "OwnableInvalidOwner",
-		"type": "error"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "address",
-				"name": "account",
-				"type": "address"
-			}
-		],
-		"name": "OwnableUnauthorizedAccount",
-		"type": "error"
-	},
-	{
-		"anonymous": false,
-		"inputs": [
-			{
-				"indexed": true,
-				"internalType": "address",
-				"name": "bettor",
-				"type": "address"
-			},
-			{
-				"indexed": false,
-				"internalType": "uint256",
-				"name": "amount",
-				"type": "uint256"
-			},
-			{
-				"indexed": false,
-				"internalType": "uint8",
-				"name": "personality",
-				"type": "uint8"
-			}
-		],
-		"name": "BetPlaced",
-		"type": "event"
-	},
-	{
-		"anonymous": false,
-		"inputs": [
-			{
-				"indexed": false,
-				"internalType": "address",
-				"name": "deployed_address",
-				"type": "address"
-			},
-			{
-				"indexed": true,
-				"internalType": "address",
-				"name": "creator",
-				"type": "address"
-			},
-			{
-				"indexed": false,
-				"internalType": "uint256",
-				"name": "startTime",
-				"type": "uint256"
-			},
-			{
-				"indexed": false,
-				"internalType": "uint256",
-				"name": "endTime",
-				"type": "uint256"
-			}
-		],
-		"name": "FightCreated",
-		"type": "event"
-	},
-	{
-		"anonymous": false,
-		"inputs": [
-			{
-				"indexed": false,
-				"internalType": "uint8",
-				"name": "winner",
-				"type": "uint8"
-			},
-			{
-				"indexed": false,
-				"internalType": "uint256",
-				"name": "totalPool",
-				"type": "uint256"
-			}
-		],
-		"name": "FightFinalized",
-		"type": "event"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "uint8",
-				"name": "winningPersonality",
-				"type": "uint8"
-			}
-		],
-		"name": "finalizeFight",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"anonymous": false,
-		"inputs": [
-			{
-				"indexed": true,
-				"internalType": "address",
-				"name": "previousOwner",
-				"type": "address"
-			},
-			{
-				"indexed": true,
-				"internalType": "address",
-				"name": "newOwner",
-				"type": "address"
-			}
-		],
-		"name": "OwnershipTransferred",
-		"type": "event"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "uint8",
-				"name": "personality",
-				"type": "uint8"
-			}
-		],
-		"name": "placeBet",
-		"outputs": [],
-		"stateMutability": "payable",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "renounceOwnership",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "address",
-				"name": "newOwner",
-				"type": "address"
-			}
-		],
-		"name": "transferOwnership",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"anonymous": false,
-		"inputs": [
-			{
-				"indexed": true,
-				"internalType": "address",
-				"name": "bettor",
-				"type": "address"
-			},
-			{
-				"indexed": false,
-				"internalType": "uint256",
-				"name": "amount",
-				"type": "uint256"
-			}
-		],
-		"name": "UserPaid",
-		"type": "event"
-	},
-	{
-		"inputs": [],
-		"name": "withdrawPlatformFees",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "currentFight",
-		"outputs": [
-			{
-				"internalType": "address",
-				"name": "roomCreator",
-				"type": "address"
-			},
-			{
-				"internalType": "uint256",
-				"name": "startTime",
-				"type": "uint256"
-			},
-			{
-				"internalType": "uint256",
-				"name": "endTime",
-				"type": "uint256"
-			},
-			{
-				"internalType": "bool",
-				"name": "isActive",
-				"type": "bool"
-			},
-			{
-				"internalType": "bool",
-				"name": "isFinalized",
-				"type": "bool"
-			},
-			{
-				"internalType": "uint8",
-				"name": "winner",
-				"type": "uint8"
-			},
-			{
-				"internalType": "uint256",
-				"name": "totalPool",
-				"type": "uint256"
-			},
-			{
-				"internalType": "uint256",
-				"name": "betAmountBot1",
-				"type": "uint256"
-			},
-			{
-				"internalType": "uint256",
-				"name": "betAmountBot2",
-				"type": "uint256"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "MIN_BET_AMOUNT",
-		"outputs": [
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "owner",
-		"outputs": [
-			{
-				"internalType": "address",
-				"name": "",
-				"type": "address"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "PLATFORM_FEE",
-		"outputs": [
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "platformFeesAccumulated",
-		"outputs": [
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	}
-]
->>>>>>> origin
+  //'0x97490eb90f2be6d6cbaf75951105ff1113779669'
 
   // if (!response.ok) {
   //   throw new Error(`Failed to place bet`);
