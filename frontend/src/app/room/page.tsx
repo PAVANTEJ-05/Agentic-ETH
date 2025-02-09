b5c7ae328f5d8e516aaabd5d49f8280326be816f
PHOENIX-05 <nptpavantej@gmail.com>
2025-02-09 18:04:12 +0530

    dot env

--- frontend/src/app/room/page.tsx
+++ frontend/src/app/room/page.tsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react";
 import Link from "next/link";
 import { v4 as uuidv4 } from "uuid";
 import { IRoom } from "@/db/models/Room";
-import Ballpit from "../components/ui/ballPit";
+// import Ballpit from "../components/ui/ballPit";
 
 // Types for the component
 interface Fighter {
